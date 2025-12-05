14f2e3872850faa9bc272a1613bc9c8d2332e98b
Mariem-Ahmed-11 <maruoma@mirax-eg.com>
2025-12-05 22:39:39 +0200

    first

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const offers = document.getElementById("offers-items");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
+const prev = document.getElementById("prev1");
+const next = document.getElementById("next1");
 let index = 0;
 function slide(o) {
     const total = offers.children.length;
@@ -61,10 +61,170 @@ prevBtn.addEventListener("click", function () {
     }
     showImage(currentIndex);
 });
-
 BoxContainer.addEventListener("click", function (event) {
     if (event.target === BoxContainer) {
         BoxContainer.style.display = "none";
     }
 });
 
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    var name = document.getElementById("name").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var subject = document.getElementById("subject").value.trim();
+    var message = document.getElementById("message").value.trim();
+
+
+    var nameError = document.getElementById("name-error");
+    var emailError = document.getElementById("email-error");
+    var subjectError = document.getElementById("subject-error");
+    var messageError = document.getElementById("message-error");
+
+
+
+    nameError.textContent = "";
+    emailError.textContent = "";
+    subjectError.textContent = "";
+    messageError.textContent = "";
+
+    var valid = true;
+
+
+    if (name === "") {
+        nameError.textContent = "Please enter your name.";
+        valid = false;
+    } else if (name.length < 3 || name.length > 15) {
+        nameError.textContent = "Name must be 3–15 characters.";
+        valid = false;
+    }
+
+
+    if (email === "") {
+        emailError.textContent = "Please enter your email.";
+        valid = false;
+    }
+
+
+    if (subject !== "" && subject.length < 3) {
+        subjectError.textContent = "Subject must be at least 3 characters.";
+        valid = false;
+    }
+
+
+    if (message !== "" && message.length < 5) {
+        messageError.textContent = "Message must be at least 5 characters.";
+        valid = false;
+    }
+
+    if (valid) {
+        alert("Form submitted successfully!");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var menuItems = [
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food1.png"
+    },
+    {
+        name: "JUMBO CRAB SHRIMP",
+        price: "$24.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food2.png"
+    },
+    {
+        name: "KOKTAIL JUICE",
+        price: "$12.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food3.png"
+    },
+    {
+        name: "CAPO STEAK",
+        price: "$60.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food4.png"
+    },
+    {
+        name: "ORGANIC FRUIT SALAD",
+        price: "$8.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food5.png"
+    },
+    {
+        name: "CHEESE PIZZA",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food6.png"
+    },
+    {
+        name: "KOFTA MEAT",
+        price: "$40.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food7.jpeg"
+    },
+    {
+        name: "SPANISH PIES",
+        price: "$14.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food8.jpeg"
+    },
+    {
+        name: "CHEESE TOST",
+        price: "$6.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food9.jpeg"
+
+    },
+    {
+        name: "FRUIT SALAD",
+        price: "$14.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food10.jpeg"
+    },
+    {
+        name: "CHICKEN SHAWARMA",
+        price: "$20.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food11.jpeg"
+    },
+    {
+        name: "MEGA CHEESE PIZZA",
+        price: "$30.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "./images/food12.jpeg"
+    }
+]
+
+
+const menuContainer = document.querySelector('.main-menue');
+menuItems.forEach(item => {
+    const div = document.createElement('div');
+    div.className = 'item';
+    div.innerHTML = `
+        <img src="${item.image}">
+        <div>
+            <h3>${item.name} <span>${item.price}</span></h3>
+            <p>${item.description}</p>
+        </div>
+    `;
+    menuContainer.appendChild(div);
+});
